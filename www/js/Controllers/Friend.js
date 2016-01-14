8854c95295dd9927724e3bd753fc8d5d436711ba
clementine <clementine.gourp@live.fr>
2016-01-14 13:34:26 +0100

    update to merge branch

--- www/js/Controllers/Friend.js
+++ www/js/Controllers/Friend.js
@@ -3,10 +3,4 @@ angular.module('friendCtrl', [])
     .controller('FriendCtrl', ['$scope', '$stateParams', 'UserService', 'Restangular', '$state',
         function($scope, $stateParams, UserService, Restangular, $state) {
 
-        $scope.user = UserService.getUser();
-            Restangular.one("users",$scope.user.id).getList("friends").then(function(friends){
-
-                $scope.friends = friends;
-
-            });
     }]);
