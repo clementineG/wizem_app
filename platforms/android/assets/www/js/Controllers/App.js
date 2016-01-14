f2c0ec056ead711155b702a4c88e3a16e58dcac4
clementine <clementine.gourp@live.fr>
2016-01-14 17:04:54 +0100

    Revert "Merge branch 'dev' of /Users/clementine/Sites/ESTEI_M2/wizem_app with conflicts."
    This reverts commit c4d7a454f6442f71a0adb4fbf83a632b701745ec.

--- platforms/android/assets/www/js/Controllers/App.js
+++
@@ -1,8 +0,0 @@
-angular.module('appCtrl', [])
-
-    .controller('AppCtrl', ['$scope', '$stateParams', 'UserService', 'Restangular', '$state',
-        function($scope, $stateParams, UserService, Restangular, $state) {
-
-        $scope.user = UserService.getUser();
-        console.log($scope.user);
-    }]);
